ea5bc3e306ca60853a0ce1eaa28e8c849fabe209
fghwer <j_olth01@uni-muenster.de>
2023-11-11 19:14:07 +0100

    foam_animation, tileset 64 x 64

--- labor.gd
+++ labor.gd
@@ -7,15 +7,19 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-func _on_button_pressed():
-	var player = "res://area_2d.tscn"
-	player.instantiate()
-	add_child(player)
-	print("player created")
+func _process(_delta):
+	#$foam_top_border.play()
+	#$TileMapFoam.play()
+	for _i in $Foam_Animation_Parent.get_children():
+		_i.play()
 	
-	pass # Replace with function body.
+
+
+
+#func _on_button_pressed():
+#	var player = "res://area_2d.tscn"
+#	player.instantiate()
+#	add_child(player)
+#	print("player created")
+	
+#	pass # Replace with function body.
