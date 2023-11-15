cf5816ea0a54b24c23c316ab1bf606067ab92518
fghwer <j_olth01@uni-muenster.de>
2023-11-15 12:42:05 +0100

    Button works, enemy spawn left + bottom every 5 s

--- nav_goblin_testscene.gd
+++ nav_goblin_testscene.gd
@@ -1,5 +1,7 @@
 extends Node
 @export var mob_scene: PackedScene
+@export var knight_scene : PackedScene
+var startpos_knight = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +11,9 @@ func _ready():
 	#add_child(player)
 	#$navigation_goblin.set_movement_target($Marker2D.position)
 	$MobTimer.start()
-	$knight_nav.startpos = $KnightSpawnLocation.position
-	$knight_nav.position = $KnightSpawnLocation.position
+	#$knight_nav.startpos = $KnightSpawnLocation.position
+	#$knight_nav.position = $KnightSpawnLocation.position
+	#get_viewport().get_mouse_position()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,12 +28,32 @@ func _process(_delta):
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	mob_spawn_location.progress_ratio = randf()
-	
-	mob.position = mob_spawn_location.position
+	var spawnpath = randi_range(0,100)
+	print(spawnpath)
+	#var mob_spawn_location = 0
+	if spawnpath < 50:
+		# Choose a random location on Path2D.
+		var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+		mob_spawn_location.progress_ratio = randf()
+		mob.position = mob_spawn_location.position
+		
+	elif spawnpath > 1:
+		# Choose a random location on Path2D.
+		var mob_spawn_location = get_node("MobPath2/MobSpawnLocation")
+		mob_spawn_location.progress_ratio = randf()
+		mob.position = mob_spawn_location.position
 	#mob.set_movement_target($Marker2D.position)
 	
 	add_child(mob)
+	
+
+
+
+func _on_spawn_knight_button_button_up():
+	var knight = knight_scene.instantiate()
+	var knight_spawn_location = get_viewport().get_mouse_position()
+	knight.position = knight_spawn_location
+	knight.startpos = knight_spawn_location
+	add_child(knight)
+	
+	
