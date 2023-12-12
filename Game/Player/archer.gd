a14461ed4b864866199570cc133e9d37c27f4e89
fghwer <j_olth01@uni-muenster.de>
2023-12-12 23:29:17 +0100

    archer

---
+++ Game/Player/archer.gd
@@ -0,0 +1,134 @@
+extends CharacterBody2D
+
+@export var maxlife = 700.
+@export var speed = 40 # How fast the player will move (pixels/sec).
+@onready var BloodParticle: GPUParticles2D = get_node("ParticleInterface/BloodParticle")
+@onready var Arrow: GPUParticles2D = get_node("ArrowInterface/GPUParticles2D")
+var startpos = Vector2.ZERO
+var life = maxlife
+var screen_size # Size of the game window.
+var i = 0
+var player_chase = false
+var player_attack = false
+var player = null
+var frame_alt = 0
+var frame_neu = 0
+
+func _physics_process(delta):
+	
+	$HPbar.set_value_no_signal(life*100/maxlife)
+	
+	if player_attack and player != null:
+		
+		velocity = Vector2.ZERO
+		if abs(player.position.x - position.x) < 10:
+			if abs(player.position.y - position.y) < 10:
+				$AnimatedSprite2D.animation = "shot_right"
+			elif player.position.y - position.y < 0:
+				$AnimatedSprite2D.animation = "shot_top"
+			else:
+				$AnimatedSprite2D.animation = "shot_bot"
+		elif (player.position.x - position.x) > 0:
+			if abs(player.position.y - position.y) < 10:
+				$AnimatedSprite2D.animation = "shot_right"
+			elif player.position.y - position.y < 0:
+				$AnimatedSprite2D.animation = "shot_top_right"
+			else:
+				$AnimatedSprite2D.animation = "shot_bot_right"
+		else:
+			if abs(player.position.y - position.y) < 10:
+				$AnimatedSprite2D.animation = "shot_right"
+			elif player.position.y - position.y < 0:
+				$AnimatedSprite2D.animation = "shot_top_right"
+			else:
+				$AnimatedSprite2D.animation = "shot_bot_right"
+			$AnimatedSprite2D.flip_h = true
+		if player.life <= 0:
+			player.hide()
+			player_attack = false
+			player_chase = false
+			player = null
+		$AnimatedSprite2D.play()
+		frame_neu = $AnimatedSprite2D.frame
+		if frame_alt != frame_neu && frame_neu == 6:
+			# && player != null:
+			if player.life != null:
+				player.life -= 100
+			player.BloodParticle.emitting = true
+			var blood_direction = Vector3.ZERO
+			var arrow_lifetime = 0
+			var arrow_rotation = 0
+			blood_direction.x = player.position.x - position.x
+			blood_direction.y = player.position.y - position.y
+			arrow_lifetime = sqrt(pow(blood_direction.x,2) + pow(blood_direction.y,2))
+			arrow_lifetime /= 800. # lifetime, 800 = velocity of arrows
+			if arrow_lifetime > 0.3: # cutoff so arrow always disappears
+				arrow_lifetime = 0.3
+			arrow_rotation = sqrt(pow(blood_direction.x,2) + pow(blood_direction.y,2))
+			arrow_rotation = (blood_direction.x/arrow_rotation)
+			arrow_rotation = acos(arrow_rotation) * 360. / (2.*PI)
+			#arrow_rotation = sin(arrow_rotation)
+			player.init_bloodparticles(blood_direction)
+			init_arrowparticles(blood_direction, arrow_lifetime, arrow_rotation)
+			Arrow.emitting = true
+			#print(acos(arrow_rotation) * 360. / (2.*PI), " ", acos(arrow_rotation)* 360. / (2.*PI), " ",atan(arrow_rotation)* 360. / (2.*PI))
+			
+			if player.life <= 0:
+				Global.Player.score += 100
+				player.queue_free()
+			#player.set_hpbar_value(player.life/player.maxlife)
+			#print(player, player.life)
+		#print(frame_alt,frame_neu)
+		frame_alt = frame_neu
+	
+	elif player_chase and player != null:
+		#print("chase")
+		velocity = (player.position - position).normalized()*speed
+		$AnimatedSprite2D.animation = "walk"
+		if(player.position.x -position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	
+	elif pow(startpos.x - position.x,2) + pow(startpos.y - position.y,2)  > 100:
+		#print("hi")
+		velocity = (startpos - position).normalized()*speed
+		$AnimatedSprite2D.animation = "walk"
+		if(startpos.x -position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	else:
+		#print("hello")
+		velocity = Vector2.ZERO
+		$AnimatedSprite2D.animation = "idle"
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	
+	move_and_slide()
+
+
+func init_bloodparticles(direction : Vector3):
+	BloodParticle.get_process_material().direction = direction
+	
+func init_arrowparticles(direction : Vector3, lifetime, arrow_rotation):
+	Arrow.get_process_material().direction = direction
+	Arrow.lifetime = lifetime
+	Arrow.get_process_material().angle_min = arrow_rotation
+	Arrow.get_process_material().angle_max = arrow_rotation
+	#Arrow.rotation = arrow_rotation
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+	
+func _on_attack_area_body_entered(body):
+	player = body
+	player_attack = true
+	player_chase = false
+
+func _on_attack_area_body_exited(body):
+	player_attack = false
+	player_chase = true
--- nav_goblin_testscene.gd
+++ nav_goblin_testscene.gd
@@ -2,18 +2,27 @@ extends Node
 @export var mob_scene: PackedScene
 @export var knight_scene : PackedScene
 @export var worker_scene : PackedScene
+@export var archer_scene : PackedScene
+@onready var SpawnKnightButtonLabel: Label = get_node("SpawnKnightButton/ButtonLayout/Label")
+@onready var SpawnWorkerButtonLabel: Label = get_node("SpawnWorkerButton/ButtonLayout/Label")
+@onready var SpawnArcherButtonLabel: Label = get_node("SpawnArcherButton/ButtonLayout/Label")
 #@onready var navigation_agent: NavigationAgent2D = get_node("mob_scene/NavigationAgent2D")
 #@export var knight_scene : PackedScene
 var startpos_knight = Vector2.ZERO
 var waiting_for_click = false
 var knight_to_spawn = null
+var archer_to_spawn = null
 var cost_knight = 2
 var cost_worker = 2
+var cost_archer = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.Player.gold = 4
 	Global.Player.score = 0
+	SpawnKnightButtonLabel.set_text(str(cost_knight))
+	SpawnWorkerButtonLabel.set_text(str(cost_worker))
+	SpawnArcherButtonLabel.set_text(str(cost_archer))
 	$MobTimer.start()
 
 
@@ -22,7 +31,7 @@ func _process(_delta):
 
 	if waiting_for_click and Input.is_action_just_pressed("click"):
 		_spawn_knight_at_mouse_position()
-		
+		_spawn_archer_at_mouse_position()
 	
 	for _i in $Foam_Animation_Parent.get_children():
 		_i.play()
@@ -31,7 +40,7 @@ func _process(_delta):
 	gold_label.set_text(str(Global.Player.gold))
 	
 	var score_label = get_node("Score")
-	score_label.set_text(str(Global.Player.score) + ": Score")
+	score_label.set_text("Score : " + str(Global.Player.score))
 	
 	
 
@@ -79,6 +88,9 @@ func _spawn_knight_at_mouse_position():
 		waiting_for_click = false
 		print("spawn")
 		Global.Player.gold -= cost_knight
+		
+		
+
 
 func _on_spawn_worker_button_pressed():
 	
@@ -100,3 +112,22 @@ func _on_spawn_worker_button_pressed():
 		add_child(worker)
 		Global.Player.gold -= cost_worker
 	
+
+
+func _on_spawn_archer_button_button_up():
+		
+	if Global.Player.gold >= cost_archer:
+		archer_to_spawn = archer_scene.instantiate()
+		waiting_for_click = true
+		
+
+func _spawn_archer_at_mouse_position():
+	if archer_to_spawn:
+		var mouse_position = get_viewport().get_mouse_position()
+		archer_to_spawn.position = mouse_position
+		archer_to_spawn.startpos = mouse_position
+		add_child(archer_to_spawn)
+		archer_to_spawn = null
+		waiting_for_click = false
+		print("spawn archer")
+		Global.Player.gold -= cost_archer		
